32f816964b2d07b732687a93959a2cdab5fc8181
randhana <pulathisi1921@gmail.com>
2023-04-15 20:07:20 +0530

    updated buttons

--- WpfMediaDemo/MainWindow.xaml.cs
+++ WpfMediaDemo/MainWindow.xaml.cs
@@ -15,7 +15,9 @@ using System.Windows.Shapes;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using Window = System.Windows.Window;
-
+using System.IO.Compression;
+using System.IO;
+using System.Diagnostics;
 
 
 namespace WpfMediaDemo
@@ -23,15 +25,15 @@ namespace WpfMediaDemo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
     
+
     public partial class MainWindow : Window
     {
         private bool isPlaying = false;
         public MainWindow()
         {
             InitializeComponent();
-            
+
             me.LoadedBehavior = MediaState.Manual;
         }
 
@@ -56,21 +58,24 @@ namespace WpfMediaDemo
                 b1.Content = "Pause";
             }
         }
-       
-  // Stop Button Code
+
+        // Stop Button Code
         private void b3_Click(object sender, RoutedEventArgs e)
         {
             // stop the running media element using same LoadedBehiour property
             me.LoadedBehavior = MediaState.Stop;
         }
 
-// Broswe Button Code
+        // Broswe Button Code
         private void b5_Click(object sender, RoutedEventArgs e)
         {
-// this will play any video / audio files dynamically
-// use openfile file dialog. WPF does not support openfile dialog. So we should add the WinForms reference
-// here. I ll show now. watch plz.
-    // create open file dialog
+
+
+
+            // this will play any video / audio files dynamically
+            // use openfile file dialog. WPF does not support openfile dialog. So we should add the WinForms reference
+            // here. I ll show now. watch plz.
+            // create open file dialog
             try
             {
                 OpenFileDialog fd = new OpenFileDialog();
@@ -80,57 +85,71 @@ namespace WpfMediaDemo
                 fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
                 // display the dialog
                 fd.ShowDialog();
-            // get the currently selected video / audio file path and store it in string variable
+                // get the currently selected video / audio file path and store it in string variable
                 string filename = fd.FileName;
-                if(filename!="")
+                if (filename != "")
                 {
-                // display this path of selected video / audio path to the text box called "tb"
+                    // display this path of selected video / audio path to the text box called "tb"
                     tb.Text = filename;
-                // now write code for the media play 
+
+
+
+                    // now write code for the media play 
                     Uri u = new Uri(filename);
-                // set this URI object to Media Element
+                    // set this URI object to Media Element
                     me.Source = u;
-                // set the volume (optional)
+                    // set the volume (optional)
                     me.Volume = 100.5;
-                // start the video using LoadedBehiour Property
+                    // start the video using LoadedBehiour Property
                     MediaState opt = MediaState.Play;
                     me.LoadedBehavior = opt;
                 }
                 else
                 {
-                    System.Windows.Forms.MessageBox.Show("No Selection","Empty");
+                    System.Windows.Forms.MessageBox.Show("No Selection", "Empty");
                 }
-            }   
-            catch(Exception e1)
+            }
+            catch (Exception e1)
             {
-                System.Console.WriteLine("Error Text: "+e1.Message);
+                System.Console.WriteLine("Error Text: " + e1.Message);
             }
         }
+
+        //Extract file from ZIP file(psw protected)
+
+
+
+
+
+
         // window_loaded event: called automatically when you run application first
         // this will be useful for preprocessing part. here video will be automatically played when run the application
         // thats y i created this event instead of button event
-        string videoURL = @"C:\Users\Krishna\Desktop\videos\morning1.mp4";
+        string videoURL = @"C:\Users\morning1.mp4";
         private void window_loaded(object sender, RoutedEventArgs e)
         {
             try
             {
-            // create URI object because MediaElement does not directly support the Video URL
+                // create URI object because MediaElement does not directly support the Video URL
                 Uri obj = new Uri(videoURL);
-            // set the local video URL via URI then attach this Uri object to Media Element using its
-            // Source property
+                // set the local video URL via URI then attach this Uri object to Media Element using its
+                // Source property
                 me.Source = obj;
-            // now url is successfully set to MediaElement. Next step is to play the Media Element
-            // this is done by MediaState built-in class
+                // now url is successfully set to MediaElement. Next step is to play the Media Element
+                // this is done by MediaState built-in class
                 MediaState opt = MediaState.Play;
-            // now attach this play state to MediaElement using its important property LoadedBehaviour
+                // now attach this play state to MediaElement using its important property LoadedBehaviour
                 me.LoadedBehavior = opt;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                System.Console.WriteLine("Error Message: "+ex.Message);
+                System.Console.WriteLine("Error Message: " + ex.Message);
             }
         }
 
-        
+
+
+
+
     }
 }
